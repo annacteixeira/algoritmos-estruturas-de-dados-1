f19b46ded1858c9fad8ac72377feb51c21d721db
Anna Chandra <annaftteixeira@gmail.com>
2025-01-29 12:18:36 -0300

    Aula 04 - Comando scanf()

---
+++ Curso - Programação Descomplicada/04-comando-scanf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    /* scanf("tipos de entrada", variaveis); */
+
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    printf("Numero digitado: %d\n", numero);
+
+    return 0;
+}
