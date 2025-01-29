c38463ae342a16c1a2cb708dab998d8a8e2f280e
Anna Chandra <annaftteixeira@gmail.com>
2025-01-29 11:35:07 -0300

    Aula 01 - Esqueleto de um programa em C

---
+++ 01-esqueleto-programa.c
@@ -0,0 +1,15 @@
+//
+// Created by anna on 29/01/2025.
+//
+
+/* Biblioteca de escrita e leitura */
+#include <stdio.h>
+
+/* Biblioteca auxiliar */
+#include <stdlib.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
+
