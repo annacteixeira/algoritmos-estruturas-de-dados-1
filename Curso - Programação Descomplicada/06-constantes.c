601543c86e49ac421819f1ce785c4624d18ac81c
Anna Chandra <annaftteixeira@gmail.com>
2025-01-29 13:19:12 -0300

    Aula 06 - Constantes

---
+++ Curso - Programação Descomplicada/06-constantes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.1415 // As constantes podem ser definidas com a diretiva #define
+
+int main(){
+    // As constantes também podem ser definidas com a palavra reservada const
+    const int nro = 10; // A constante é uma variável que não pode ser alterada
+
+    return 0;
+}
