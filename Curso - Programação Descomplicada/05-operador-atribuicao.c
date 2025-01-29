09d662b6ac225c939240eee8e22199c447b78ee4
Anna Chandra <annaftteixeira@gmail.com>
2025-01-29 12:31:37 -0300

    Aula 05 - Operador de atribuição

---
+++ Curso - Programação Descomplicada/05-operador-atribuicao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // variável = expressão;
+
+    int x = 5; // Declaração atributiva
+    int y;
+    y = x;
+    y = x + 10;
+
+    return 0;
+}
