7f65a3fa03eb976dbb00696e68a953c6d9860cfd
Anna Chandra <annaftteixeira@gmail.com>
2025-02-09 16:49:12 -0300

    Aula 20 - Aninhamento de Repetições

---
+++ Curso - Programação Descomplicada/20-aninhamento-de-repeticoes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    /*
+        Aninhamento de repetições é a prática de colocar uma estrutura de repetição dentro de outra.
+        Podemos aninhar qualquer tipo de estrutura de repetição (while, do-while, for).
+        O aninhamento de repetições é útil quando queremos repetir um bloco de comandos várias vezes,
+        e dentro desse bloco de comandos queremos repetir outro bloco de comandos várias vezes.
+
+        Exemplo:
+        repeticao1(condicao1) {
+            bloco de comandos 1;
+            repeticao2(condicao2) {
+                bloco de comandos 2;
+                repeticao...
+            }
+        }
+    */
+
+    return 0;
+}
