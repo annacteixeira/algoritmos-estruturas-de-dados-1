99655daddd32c107cf4166098a1a02c3c7128eba
Anna Chandra <annaftteixeira@gmail.com>
2025-02-21 11:25:46 -0300

    Lista 01 - Questão 23

---
+++ Listas de Exercicio/Lista 01/Parte 03/questao-23.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Elabore  um  programa  que  contenha  uma  constante  qualquer  do  tipo  int.  Use  o  comando 
+    const. Imprima essa constante.
+*/
+
+int main()
+{
+    const int PI = 3;
+
+    printf("%d", PI);
+    return 0;
+}
