952c3c4fed11c1e4323e9cc84d8da580131beace
Anna Chandra <annaftteixeira@gmail.com>
2025-02-21 11:12:44 -0300

    Lista 01 - Questão 13

---
+++ Listas de Exercicio/Lista 01/Parte 03/questao-13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Escreva um programa que leia um número inteiro e depois o imprima
+*/
+
+int main()
+{
+    int n = 0;
+
+    printf("\nInforme um numero inteiro: ");
+    scanf("%d", &n);
+
+    printf("\n%d", n);
+    return 0;
+}
