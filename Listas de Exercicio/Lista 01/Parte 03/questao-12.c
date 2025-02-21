b755c081e47293ebf75d371c5ebeeb2c9d23b8f2
Anna Chandra <annaftteixeira@gmail.com>
2025-02-21 11:11:39 -0300

    Lista 01 - Questão 12

---
+++ Listas de Exercicio/Lista 01/Parte 03/questao-12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Elabore um programa que escreva as mensagens “Início do programa” e “Fim” na tela, uma 
+    em cada linha, usando apenas um comando printf(). 
+*/
+
+int main()
+{
+    printf("\nInicio do programa\nFim\n");
+    return 0;
+}
