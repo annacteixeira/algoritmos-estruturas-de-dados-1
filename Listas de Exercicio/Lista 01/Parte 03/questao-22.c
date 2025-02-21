eb7593d9b47adfd1fff53869b8a35d6567a310e7
Anna Chandra <annaftteixeira@gmail.com>
2025-02-21 11:24:12 -0300

    Lista 01 - Questão 22

---
+++ Listas de Exercicio/Lista 01/Parte 03/questao-22.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.141596
+
+/*
+    Elabore um programa que contenha uma constante qualquer do tipo float. Use  o comando 
+    #define. Imprima essa constante. 
+*/
+
+int main()
+{
+    printf("%f", PI);
+    return 0;
+}
